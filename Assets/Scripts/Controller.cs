3a7f04445bec493f8cebb4f259729f02b70c64ed
Vedant P <vedantpatwari01@gmail.com>
2016-04-10 01:14:04 +0530

    Added character controller
    Added basic character controller

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	public float moveSpeed; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position += new Vector3 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime);
+	
+	}
+}
