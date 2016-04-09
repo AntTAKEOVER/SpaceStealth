9f9687db4f808de7f23844f6b6eea9ffe2e4036f
Vedant P <vedantpatwari01@gmail.com>
2016-04-09 19:46:58 +0530

    Initialisation Commit
    Initialise project.

---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldOfView : MonoBehaviour {
+
+
+	public float viewRadius;
+	[Range(0,360)]
+	public float viewAngle;
+
+	public LayerMask targetMask;
+	public LayerMask obstacleMask;
+
+	void FindVisibleTargets(){
+		Collider[] targetsInViewRadius = Physics.OverlapSphere (transform.position, viewRadius, targetMask);
+
+		for(int i = 0; i < targetsInViewRadius.Length; i++){
+			Transform target = targetsInViewRadius [i].transform;
+			Vector3 dirToTarget = (target.position - transform.position).normalized;
+			if (Vector3.Angle (transform.forward, dirToTarget) < viewAngle / 2) {
+				float dstToTarget = Vector3.Distance (transform.position, target.position);
+
+				if (!Physics.Raycast (transform.position, dirToTarget, dstToTarget, obstacleMask)) {
+					
+				}
+			}
+		}
+	}
+		
+
+	public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal){
+		if (!angleIsGlobal) {
+			angleInDegrees += transform.eulerAngles.y;
+		}
+		return new Vector3 (Mathf.Sin (angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos (angleInDegrees * Mathf.Deg2Rad));
+
+	}
+}
