4d2284b20cd9e0548def7701f793655b026ea105
Vedant P <vedantpatwari01@gmail.com>
2016-04-10 02:49:14 +0530

    Minor Changes to FOV
    Some minor tweaks to the FOV

--- Assets/Editor/FieldOfViewEditor.cs
+++ Assets/Editor/FieldOfViewEditor.cs
@@ -15,7 +15,7 @@ public class FieldOfViewEditor : Editor {
 		Handles.DrawLine (fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
 		Handles.DrawLine (fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
 
-		Handles.color = Color.red;
+		Handles.color = Color.white;
 		foreach (Transform visibleTarget in fow.visibleTargets) {
 			Handles.DrawLine (fow.transform.position, visibleTarget.position);
 		}
